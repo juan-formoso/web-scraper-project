8182e54d2aaebe826eec47eb307200940b7df2ce
Juan Formoso <trajano.juan@gmail.com>
2022-03-16 10:25:05 -0300

    Refatorando app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,45 @@
 import express, { Request, Response } from "express";
 import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 const connectionString = process.env.DATABASE_URL || "";
 const connection = mysql.createConnection(connectionString);
 connection.connect();
 
-app.get("/api/characters", (req: Request, res: Response) => {
+app.get("/api/characters", (_req: Request, res: Response) => {
   const query = "SELECT * FROM Characters";
   connection.query(query, (err, rows) => {
     if (err) throw err;
+
     const retVal = {
       data: rows,
-      message: rows.length === 0 ? "No characters found" : null,
+      message: "",
     };
-    return res.send(retVal);
+    if (rows.length === 0) {
+      retVal.message = "No records found";
+    }
+    res.send(rows);
   });
 });
 
-app.get("/api/characters:id", (req: Request, res: Response) => {
-  const id = req.params.id;
-  const query = `SELECT * FROM Characters WHERE id = ${id}`;
+app.get("/api/characters/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM Characters WHERE ID = "${id}" LIMIT 1`;
   connection.query(query, (err, rows) => {
     if (err) throw err;
+
     const retVal = {
       data: rows.length > 0 ? rows[0] : null,
       message: rows.length === 0 ? "No Record Found" : "",
     };
-    return res.send(retVal);
+    res.send(retVal);
   });
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Throne of Glass API is Running! 👍");
 });
